ebedada07084ad8e1f52bfae830d394eb0e2ea96
Pat Nakajima <patnakajima@gmail.com>
2009-04-02 23:30:24 -0400

    Added ability proxy to multiple targets and :unless

--- lib/proxen.rb
+++ lib/proxen.rb
@@ -1,19 +1,39 @@
 module Proxen
-  def proxy_to(target, options={})
-    if options[:blank_slate]
-      class_eval do
-        instance_methods.each do |sym|
-          undef_method(sym) unless sym.to_s =~ /__/
-        end
+  def self.blankify(klass)
+    klass.class_eval do
+      instance_methods.each do |sym|
+        undef_method(sym) unless sym.to_s =~ /__/
       end
     end
-    
-    cond = options[:if] ? "sym.to_s =~ #{options[:if].inspect}" : "true"
-    
+  end
+
+  def self.conditional(options)
+    case
+    when options[:if]
+      "sym.to_s =~ #{options[:if].inspect}"
+    when options[:unless]
+      "! (sym.to_s =~ #{options[:unless].inspect})"
+    else
+      "true"
+    end
+  end
+
+  def proxy_to(*targets)
+    options = targets.last.is_a?(Hash) ? targets.pop : {}
+    targets = Array(targets).flatten
+
+    Proxen.blankify(self) if options[:blank_slate]
+
     class_eval(<<-END, __FILE__, __LINE__)
       def method_missing(sym, *args, &block)
-        if #{cond}
-          #{target}.send(sym, *args, &block)
+        super unless #{Proxen.conditional(options)}
+
+        receiver = #{targets.inspect}.detect do |t|
+          __send__(t).respond_to?(sym)
+        end
+
+        if receiver
+          __send__(receiver).send(sym, *args, &block)
         else
           super
         end
--- spec/proxen_spec.rb
+++ spec/proxen_spec.rb
@@ -5,47 +5,47 @@ describe Proxen do
   it "proxies method_missing to method" do
     @klass = Class.new do
       proxy_to :foo
-      
+
       def foo
         Class.new { def bar; :proxied end }.new
       end
     end
-    
+
     @klass.new.bar.should == :proxied
   end
-  
+
   it "respects other method missings" do
     @klass = Class.new do
       proxy_to :foo
-      
+
       def foo
         Class.new { def bar; :proxied end }.new
       end
-      
+
       def method_missing(sym, *args, &block)
         :not_proxied
       end
     end
-    
+
     @klass.new.bar.should == :not_proxied
   end
-  
+
   it "can generate blank slates" do
     @klass = Class.new do
       proxy_to :foo, :blank_slate => true
-      
+
       def foo
         Class.new { def inspect; :proxied end }.new
       end
     end
-    
+
     @klass.new.inspect.should == :proxied
   end
-  
-  it "accepts regexen" do
+
+  it "accepts :if regexen" do
     @klass = Class.new do
       proxy_to :foo, :if => /zz/
-      
+
       def foo
         Class.new {
           def baz; :err end
@@ -54,12 +54,50 @@ describe Proxen do
         }.new
       end
     end
-    
+
     @klass.new.fizz.should == :proxied
     @klass.new.buzz.should == :proxied
-    
+
     proc {
       @klass.new.baz.should == :proxied
     }.should raise_error(NoMethodError)
+  end
+
+  it "accepts :unless regexen" do
+    @klass = Class.new do
+      proxy_to :foo, :unless => /zz/
+
+      def foo
+        Class.new {
+          def baz; :proxied end
+          def fizz; :err end
+          def buzz; :err end
+        }.new
+      end
+    end
+
+    @klass.new.baz.should == :proxied
+
+    proc {
+      @klass.new.fizz
+      @klass.new.buzz
+    }.should raise_error(NoMethodError)
+  end
+
+  it "proxies to multiple" do
+    @klass = Class.new do
+      proxy_to :foo, :bar
+
+      def foo
+        Class.new { def fizz; :proxied end }.new
+      end
+
+      def bar
+        Class.new { def buzz; :proxied end }.new
+      end
+    end
+
+    @klass.new.fizz.should == :proxied
+    @klass.new.buzz.should == :proxied
   end
 end
